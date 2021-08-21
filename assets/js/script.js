d1f8b9043820ea8579c83d057775c4ee85c9fad3
Greg Riveroll <griveroll86@gmail.com>
2021-08-21 19:32:13 -0400

    url links fixed?

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,13 +131,13 @@ function checkAnswer(){
 
 function checkEndGame(){
     if (currentQuestion >= 5){
-        window.location.replace("https://griveroll86.github.io/quiz/highscores.html")
+        window.location.href = "https://griveroll86.github.io/quiz/highscores.html";
     } 
 }
 
 function gameOver() {
     alert("You ran out of time.")
-    window.location.replace("https://griveroll86.github.io/quiz/highscores.html")
+    window.location.href = "https://griveroll86.github.io/quiz/highscores.html";
 }
 
 document.getElementById("startBtn").addEventListener("click", startGame);
